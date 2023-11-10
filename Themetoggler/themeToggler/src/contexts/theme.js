5a88886a93192d71e74ed6a39608fa90fccc4b16
[Vidit Pandey] <viditpandey06@gmail.com>
2023-11-10 11:54:57 +0530

    Theme Toggler
    Added dark mode and light mode using context in React

---
+++ Themetoggler/themeToggler/src/contexts/theme.js
@@ -0,0 +1,11 @@
+import { createContext, useContext } from "react";
+export const ThemeContetxt = createContext({
+  themeMode: "light",
+  darkTheme: () => {},
+  lightTheme: () => {},
+});
+
+export const ThemeProvider=ThemeContetxt.Provider
+export default function useTheme(){
+    return useContext(ThemeContetxt)
+}
