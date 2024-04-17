6e7ba31dd34a0c01d26364c5305b4c90c476ff98
[Vidit Pandey] <viditpandey06@gmail.com>
2024-04-17 21:15:03 +0530

    Fix propValues

--- ReactComponents/Components/src/App.jsx
+++ ReactComponents/Components/src/App.jsx
@@ -1,19 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Card from '../components/card'
 
 function App() {
-  const [count, setCount] = useState(0)
+ //const [count, setCount] = useState(0)
   
   return (
     <>
-     <Card username='Windows' userprofile='This is a eample for object destructuring but for windows'/>
-      <Card username='Linux' userprofile='This is a eample for object destructuring but for Linux' />
-      <Card username='Tapasya' userprofile='dyebeuvbevbyevbeivbaeivbaeyfbaeyj fwfe'/>
-      <Card username='Tapasya' userprofile='dyebeuvbevbyevbeivbaeivbaeyfbaeyj fwfe' link='https://images.pexels.com/photos/9076961/pexels-photo-9076961.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' />
-    </>
+     <Card username='Windows' userprofile='This is a eample1 for object destructuring but for windows' link='google.com'/>
+      <Card username='Linux' userprofile='This is a eample2 for object destructuring but for Linux' />
+      <Card username='Vidit' userprofile='dyebeuvbevbyevbeivbaeivbaeyfbaeyj fwfe'/>
+ </>
   )
 }
 
